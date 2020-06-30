3ab5449bc469fdc10c169b9e739cd07977be8d6c
Hyounsung <67617248+Hyounsung@users.noreply.github.com>
2020-06-30 13:28:29 +0900

    first

---
+++ GithubPractice/src/forloop/Helloworld.java
@@ -0,0 +1,12 @@
+package forloop;
+import java.lang.*;
+import java.util.*;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		for(int i = 0; i<5; i++) {
+		System.out.println("Helloworld");
+		}
+	}
+
+}

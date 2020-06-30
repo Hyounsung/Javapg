e98f211674da2ad7b06339303634316afb53fadf
Hyounsung <67617248+Hyounsung@users.noreply.github.com>
2020-06-30 13:32:44 +0900

    hat

---
+++ GithubPractice/src/forloop/hat.java
@@ -0,0 +1,11 @@
+package forloop;
+import java.lang.*;
+import java.util.*;
+
+public class hat {
+	public static void main(String[] args) {
+		String str = "hat";
+		System.out.println(str);
+	}
+
+}
